06010157848924c30a1c40cad67799d8693f40dc
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-10-09 16:35:41 -0300

    implementada a camada controller

---
+++ InstrumentoControllers/strumentoController.cs
@@ -0,0 +1,19 @@
+﻿using CrudInstrumento.Servicos;
+using CrudInstrumento.Core.Entidades;
+using System.Runtime.CompilerServices;
+namespace CrudInstrumento.InstrumentoControllers
+{
+    internal class strumentoController
+    {
+        private InstrumentoService _instrumentoService;
+        public void InserirInstrumento(string nome)
+        {
+            Instrumento instrumento = new Instrumento()
+            {
+                Nome = nome
+            };
+            _instrumentoService = new InstrumentoService();
+            _instrumentoService.Add(instrumento);
+        }
+    }
+}
