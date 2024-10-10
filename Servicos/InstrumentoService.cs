c44b5e42de50bac1cbb4cf57fec27410032dc377
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-10-10 10:53:57 -0300

    Criada uma classe para as verificações

--- Servicos/InstrumentoService.cs
+++ Servicos/InstrumentoService.cs
@@ -5,30 +5,48 @@ namespace CrudInstrumento.Servicos
 {
     internal class InstrumentoService
     {
-        private InstrumentoRepositorio _instrumentoRepositorio;
+        private InstrumentoRepositorio _instrumentoRepositorio = new InstrumentoRepositorio();
 
         public void Add(Instrumento instrumento)
         {
-            _instrumentoRepositorio = new InstrumentoRepositorio();
+            
+            Validadores.ValidarNomeInstrumento(instrumento.Nome);
+
             _instrumentoRepositorio.Add(instrumento);
             Console.WriteLine("Instrumento inserido no sistema");
         }
+
         public List<Instrumento> GetAll()
         {
             return _instrumentoRepositorio.GetAll();
         }
+
         public void EditarInstrumento(int id, string novoNome)
         {
-            _instrumentoRepositorio = new InstrumentoRepositorio();
+            
+            Validadores.ValidarIdInstrumento(id);
+            Validadores.ValidarNomeInstrumento(novoNome);
+
             var instrumento = _instrumentoRepositorio.GetAll().FirstOrDefault(i => i.Id == id);
+
+
+            Validadores.ValidarInstrumentoExistente(instrumento);
+
             instrumento.Nome = novoNome;
             _instrumentoRepositorio.Update(instrumento);
             Console.WriteLine("Item atualizado");
         }
+
         public void Delete(int id)
         {
-            _instrumentoRepositorio = new InstrumentoRepositorio();
+            
+            Validadores.ValidarIdInstrumento(id);
+
             var instrumento = _instrumentoRepositorio.GetAll().FirstOrDefault(i => i.Id == id);
+
+
+            Validadores.ValidarInstrumentoExistente(instrumento);
+
             _instrumentoRepositorio.Delete(instrumento);
             Console.WriteLine("Item removido");
         }
---
+++ Servicos/validadores.cs
@@ -0,0 +1,42 @@
+﻿using CrudInstrumento.Core.Entidades;
+
+namespace CrudInstrumento.Servicos
+{
+    internal static class Validadores
+    {
+        // Validação para o nome do instrumento
+        public static void ValidarNomeInstrumento(string nome)
+        {
+            if (string.IsNullOrWhiteSpace(nome))
+            {
+                Console.WriteLine("O nome do instrumento não pode ser vazio ou nulo.");
+                throw new ArgumentException("Nome inválido");
+            }
+            else if (nome.Length <= 4)
+            {
+                Console.WriteLine("O nome do instrumento deve ter mais que 4 caracteres.");
+                throw new ArgumentException("Nome muito curto");
+            }
+        }
+
+        // Validação para o ID do instrumento
+        public static void ValidarIdInstrumento(int id)
+        {
+            if (id < 0)
+            {
+                Console.WriteLine("O ID do instrumento deve ser maior ou igual a 0.");
+                throw new ArgumentException("ID inválido");
+            }
+        }
+
+        // Validação se o instrumento existe
+        public static void ValidarInstrumentoExistente(Instrumento instrumento)
+        {
+            if (instrumento == null)
+            {
+                Console.WriteLine("Instrumento não encontrado.");
+                throw new ArgumentException("Instrumento inexistente");
+            }
+        }
+    }
+}
